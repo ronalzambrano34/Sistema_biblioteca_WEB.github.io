18fc09ea178de1f9350530adcab681ca89c2158c
ronalzambrano34 <ronalzambrano34@gmail.com>
2023-03-26 02:44:08 -0400

    fix header

--- empleados/empleados.php
+++ empleados/empleados.php
@@ -1,29 +1,5 @@
- <?php
-    session_start();
-    $id=$_SESSION['Id_usuario'];
-    $usuario=$id;
-    if ($id == null || $id='') {
-        header("location:../index.php");
-    }
-  ?>
-<!DOCTYPE html>
-<html>
+ <?php require_once("../head_files.php") ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Librería Catedral</title>
-
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="../vendor/bootstrap/css/style.css">
-    <link rel="stylesheet" type="text/css" href="../icofont/icofont.min.css">
-    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/sweetalert.css">
-    <script src="../vendor/bootstrap/js/sweetalert.min.js" type="text/javascript"></script>
-</head>
 
 
             <ul class="list-unstyled components">
@@ -135,63 +111,7 @@
                 </li>
             </ul>
         </nav>
-         <!-- Page Content  -->
-        <div class="menu">
-            <nav style="background-color:#00A0DE" class="p-2 navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <i class="fas fa-align-left"></i>
-                    <a href="#"><span id="sidebarCollapse" class="text-white h3 icofont-navigation-menu"></span></a>    
-                    <div class="ml-3 text-center text-white">
-                        <div class="spinner-grow text-light" role="status">
-                          <span class="sr-only">Loading...</span>
-                        </div>
-                    </div> 
-                    <button class="btn d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fas fa-align-justify"></i>
-                        <span class="text-white h3 icofont-circled-down"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a data-toggle="modal" data-target="#exampleModalScrollable1" class="text-white h5 nav-link" href="#" title="Nuestra empresa"><i class="mr-2 icofont-building-alt"></i></a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a data-toggle="modal" data-target="#exampleModalScrollable" class="text-white h5 nav-link" href="#" title="Contactanos"><i class="mr-2 icofont-search-map"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="text-white h5 nav-link" href="../inicio.php"><i class="icofont-ui-home" title="Inicio"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../prestamos/pendientes.php" class="text-white h5 nav-link" href="#"><i class="icofont-notification" title="Notificaciones"><span style="position: relative; top: -8px;" class="bg-warning badge count">
-                                      <?php 
-                                        require_once("../conexion/conexion.php");
-                                        $buscar_pend="SELECT COUNT(Id_prestamo) AS numero FROM prestamos WHERE Fecha_devolucion<NOW() AND Estatus='Pendiente'";
-                                        $confirmar=$conexion->query($buscar_pend);
-                                        $rows=$confirmar->fetch_assoc();
-                                        echo $rows['numero'];;
-                                        ?>
-                                </span></i></a>
-                            </li>
-                            <!-- Example single danger button -->
-                            <li class="nav-item">
-                                <div class="btn-group">
-                                  <button type="button" id="perfil" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img width="43" height="43" src="../images/user.png" alt="">
-                                  </button>
-                                 <div class="dropdown-menu dropdown-menu-right">
-                                    <a href="../usuarios/perfil.php"><button class="dropdown-item" type="button">Actualizar perfil</button></a>
-                                    <a href="../usuarios/modificar_contrasena.php"><button class="dropdown-item" type="button">Cambiar contraseña</button></a>
-                                    <div class="dropdown-divider"></div>
-                                    <a href="../conexion/cerrar_sesion.php"><button class="dropdown-item" type="button">Cerrar sesión</button></a>
-                                </div>
-                            </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+         
         <div class="container table-responsive">
             <br><br><br><br>
             <form action="#" class="form" method="POST">
