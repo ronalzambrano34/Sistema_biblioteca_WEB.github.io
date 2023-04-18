e9bded9485022abcdbf8c38156cedce21c26f84d
ronalzambrano34 <ronalzambrano34@gmail.com>
2023-04-17 23:12:54 -0400

    llevando el modal hacia el footer, para q sea 1 solo

--- footer.php
+++ footer.php
@@ -34,3 +34,129 @@
   </div>
   <!-- /.container -->
 </footer>
+
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">Contáctanos</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body bg-light">
+        <div class="row">
+          <div class="col-sm-6 p-2">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-envelope h1"></span>
+                <br>
+                <small>Name: Ronal Zambrano Ferrer.</small>
+                <br>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 p-2">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-map h1"></span>
+                <br>
+                <small>Pais: Cuba</small>
+                <br>
+                <small>Provincia: Santiago de Cuba</small>
+                <br>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 p-2">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-envelope h1"></span>
+                <br>
+                <small>Email: ronalzambrano34@gmail.com</small>
+                <br>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 p-2">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-brand-whatsapp h1"></span>
+                <br>
+                <small>Tel: +53 53843778</small>
+                <br>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="exampleModalScrollable1" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">Quiénes somos</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body bg-light">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-hat h1"></span>
+                <p class="card-title"><b>Misión</b></p>
+                <small>Nuestra misión es poder dar a conocer toda la sabiduría a través de
+                  nuestros
+                  libros. Tener un repertorio digno para todas las personas; clases sociales,
+                  edades, grados y campos de estudio. Que nuestros libros sean del mayor
+                  agrado de
+                  nuestros visitadores, contando la mejor calidad de servicio en préstamos de
+                  títulos. Siempre con el cello de la casa.</small>
+                <br>
+              </div>
+            </div>
+          </div>
+          <br>
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-eye h1"></span>
+                <p class="card-title"><b>Visión</b></p>
+                <small>Nuestra visión es siempre tener una atención del público a pesar
+                  del
+                  tiempo en el que estamos, ser una de las instituciones de títulos literarios
+                  más
+                  conocidos del mundo. Tener instalaciones de calidad para preservar el buen
+                  espacio para leer, contar con el mejor trato de visitante-empleado, ya que
+                  nuestro público lo merece.</small>
+                <br>
+              </div>
+            </div>
+          </div>
+          <br>
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-body">
+                <span class="text-info icofont-chart-histogram-alt h1"></span>
+                <p class="card-title"><b>Objetivo General</b></p>
+                <small>Tener un sistema para poder llevar a cabo la administración de los
+                  registros
+                  que se generan día con día y hacer más fácil la búsqueda de visitantes, las
+                  personas que tienen préstamos y los adeudos de libros. También llevar un
+                  registro de los libros que puedan estar dañados y así hacer una petición de
+                  cambios.</small>
+                <br>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
